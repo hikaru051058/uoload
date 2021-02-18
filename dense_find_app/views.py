c50db55f0467448f595a823ddadd0010a14366d3
Your Name <you@example.com>
2021-02-18 02:47:22 +0000

    add css

--- dense_find_app/views.py
+++ dense_find_app/views.py
@@ -4,6 +4,6 @@ from django.views.generic import ListView, DetailView, TemplateView
 # Create your views here.
 
 class HomeView(TemplateView):
-    template_name = "dense_find_app/index.html" #dense_find_app/templates/dense_find_app/index.html
+    template_name = "dense_find_app/index.html"  #dense_find_app/templates/dense_find_app/index.html
     
     
